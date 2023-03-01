dc30487b6dd34543c2e1f7e46909921d2c3aa44b
Zloprcek <frantafajna@gmail.com>
2023-03-01 12:17:00 +0100

    Upravil jsem Routu

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return <BrowserRouter>
 
   <Routes>
-    <Route path="/" element={ <Home /> } />
+    <Route path="testing" element={ <Home /> } />
     <Route path="/Skills" element={ <Skills /> } />
     <Route path="/contact" element={ <Contact /> } />
     <Route path="*" element={ <Error/> } />
