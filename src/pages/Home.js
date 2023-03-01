d5faa8d3ec7eb9eda08a9787210a3fa8a87c4d24
Zloprcek <frantafajna@gmail.com>
2023-03-01 16:07:33 +0100

    Fade Animace

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Fade from 'react-reveal/Fade';
 function Home() {
   return <>
   <Navbar/>
+  <Fade left>
   <h1>Domovská stránka</h1>
+  </Fade>
   </>
 }
 export default Home
