36adbb65840e5b67569f0710e8752846beaaf846
Zloprcek <frantafajna@gmail.com>
2023-03-01 12:06:59 +0100

    Upravil jsem skills

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-import movies from "../components/data"
-import {Link} from "react-router-dom"
 function Skills() {
     return (
      <div>
