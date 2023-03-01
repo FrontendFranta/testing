4beefde0c5f5b5000bdec2da3be208cb4c613ac1
Zloprcek <frantafajna@gmail.com>
2023-03-01 12:06:16 +0100

    Upravil jsem skills

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -4,22 +4,10 @@ import movies from "../components/data"
 import {Link} from "react-router-dom"
 function Skills() {
     return (
-        <>
+     <div>
         <Navbar></Navbar>
         <h1>Dovednosti</h1>
-        <div>
-            {movies.map((oneMovie) =>{
-return <article key={oneMovie.id}>
-    <h2>{oneMovie.title}</h2>
-    <img src={oneMovie.image}></img><br/>
-    <Link to={`/all-skills/${oneMovie.id}`}>Více informací</Link>
-    
-</article>
-            
-
-} )}
         </div>
-        </>
       )
 }
 
